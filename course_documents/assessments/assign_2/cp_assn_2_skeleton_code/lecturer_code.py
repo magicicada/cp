0b1afbd604bab3bd9a243b2a1ca682512c43aa52
Jess Enright <jenright@gmail.com>
2024-11-05 16:59:23 +0000

    Adding two instances with known burn quantities

--- course_documents/assessments/assign_2/cp_assn_2_skeleton_code/lecturer_code.py
+++ course_documents/assessments/assign_2/cp_assn_2_skeleton_code/lecturer_code.py
@@ -3,6 +3,23 @@ import networkx as nx
 import submitted_solution
 
 
+# height + 1 should burn
+# returns start, networkx graph
+def generate_binary_tree_instance(height):
+  arity = 2
+  tree = nx.balanced_tree(arity, height)
+  # find the root
+  root = 0
+  assert tree.degree(root) == arity
+  return root,tree
+
+# four should burn
+# returns start, networkx graph
+def generate_ladder_instance():
+  height = 20
+  ladder = nx.ladder_graph(height)
+  return int(height/2), ladder
+
 
 def run_trial(graph, start, timeout=5000):
    ilp_result = submitted_solution.run_ilp(graph, start_node=start, timeout=timeout)
@@ -16,6 +33,7 @@ def skeleton_runs():
    cp, ilp = run_trial(graph, start = 5)
    runs_results['path_start_mid'] = {'cp': cp, 'ilp': ilp}
 
+# in a complete graph all but one vertex should burn
    graph = nx.complete_graph(12)
    cp, ilp = run_trial(graph, start = 0)
    runs_results['complete'] = {'cp': cp, 'ilp': ilp}
